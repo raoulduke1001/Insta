0c216a8296d3f3b6afc2ca19838bc03c7ec1bbe4
raoulduke1001 <1raoulduke1@gmail.com>
2020-01-28 16:43:04 +0300

    0.25

---
+++ src/components/Users.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import User from './User';
+
+
+export default function Users(){
+    return(
+        <div className="right">
+        <User 
+            src="https://static.bershka.net/4/photos2/2019/V/0/1/p/1966/381/250/1966381250_1_1_3.jpg?t=1554885223409" 
+            alt="girl" 
+            name="beauty"/>
+            <div className="users__block">
+            <User 
+                src="https://static.bershka.net/4/photos2/2019/V/0/1/p/1966/381/250/1966381250_1_1_3.jpg?t=1554885223409" 
+                alt="girl" 
+                name="beauty"
+                min/>
+            <User 
+                src="https://static.bershka.net/4/photos2/2019/V/0/1/p/1966/381/250/1966381250_1_1_3.jpg?t=1554885223409" 
+                alt="girl" 
+                name="beauty"
+                min/>
+            <User 
+                src="https://static.bershka.net/4/photos2/2019/V/0/1/p/1966/381/250/1966381250_1_1_3.jpg?t=1554885223409" 
+                alt="girl" 
+                name="beauty"
+                min/>
+            <User 
+                src="https://static.bershka.net/4/photos2/2019/V/0/1/p/1966/381/250/1966381250_1_1_3.jpg?t=1554885223409" 
+                alt="girl" 
+                name="beauty"
+                min/>
+            </div>
+
+
+
+        </div>
+
+
+
+
+
+    )
+
+
+
+
+
+}
